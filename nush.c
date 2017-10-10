dc5308467739d57cb81ee07277a5c1ee4bd3f222
EVAN <johnson.ev@husky.neu.edu>
2017-10-09 21:16:40 -0400

    Added cd, exit, and reading script files

--- nush.c
+++ nush.c
@@ -7,62 +7,66 @@
 
 #include "tokens.h"
 void
-execute(char* cmd, strarr* arr)
+execute(strarr* arr)
 {
     int cpid;
 
-    if ((cpid = fork())) {
-        char* args[arr->size];
+    if (strcmp(strarr_get(arr, 0), "cd") == 0) {
+      chdir(strarr_get(arr, 1));
+    }
 
-        // add individual tokens to an args array.
-        for (int i = 0; i < arr->size; i++) {
-          args[i] = strarr_get(arr, i);
-        }  
+    // if the inputted command is "exit"
+    else if (strcmp(strarr_get(arr, 0), "exit") == 0) {
+      exit(0);
+    }
+    
+    else if ((cpid = fork())) {
+      int status;
 
-        int status;
-        waitpid(cpid, &status, 0);
-
-        if (WIFEXITED(status)) {
-          // TODO: error code reporting
-        }
+      waitpid(cpid, &status, 0);
+        //printf("%d", status);
     }
     else {
-        // child process
-        char* args[arr->size];
+      // child process
+      char* args[arr->size];
 
-        for (int i = 0; i < arr->size; i++) {
-          args[i] = strarr_get(arr, i);
-        }
+      for (i = 0; i < arr->size; i++) {
+        args[i] = strarr_get(arr, i); 
+      }
 
-        execvp(args[0], args);
+      args[arr->size] = NULL; // null terminate array
+      execvp(args[0], args);
     }
 }
 
-// tokenize cmd and pass that into the execute function
 int
 main(int argc, char* argv[])
 {
     char cmd[256];
+    FILE* fp;
 
+    // if nush is in interactive mode
     if (argc == 1) {
-      while(1) {
+      fp = stdin;
+    }
+
+    // nush was given a script as an argument
+    else { 
+      fp = fopen(argv[1], "r");
+    }
+
+    while(1) {
+      if (argc == 1) {
         printf("nush$ ");
-        fflush(stdout);
-
-        char* rv = fgets(cmd, 256, stdin);
-        if (!rv) {
-          break;
-        }
-        strarr* arr = get_tokens(cmd);
-        execute(cmd, arr);
       }
-    }
-    else {
-        memcpy(cmd, "echo", 5);
-    }
+      fflush(stdout);
 
-//    strarr* arr = get_tokens(cmd);
-//    execute(cmd, arr);
-
+      char* rv = fgets(cmd, 256, fp);
+      if (!rv) {
+        break;
+      }
+      strarr* arr = get_tokens(cmd);
+      execute(arr);
+    }
     return 0;
 }
